56426439bdd1fb8e5454aaa5d394668ad7b442a6
Yves Chartier <Yves.Chartier@ec.gc.ca>
2002-02-22 14:47:00 +0000

    Ajout du convertisseur de dictionnaire .recrc en format xml

---
+++ convert-dict/dict.c
@@ -0,0 +1,155 @@
+/* RMNLIB - Library of useful routines for C and FORTRAN programming
+ * Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
+ *                          Environnement Canada
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <malloc.h>
+#include <rec.h>
+#include <rpnmacros.h>
+   
+static int nbChampsDict = 0;
+_InfoChamps *infoChamps = NULL;
+FILE *fdict;
+
+void f77name(initxmldict)(char nomfich[], int lennomfich)
+{
+  char nomDict[128];
+
+  nomfich[lennomfich] = '\0';
+  nettoyer(nomfich);
+  strcpy(nomDict, nomfich);
+  if (0 == strcmp(nomfich, "stdout"))
+    {
+    fdict = stdout;
+    }
+  else
+    {
+    fdict = fopen(nomDict, "w");
+    if (fdict == NULL)
+      {
+      fprintf(stderr, "Pas capable d'ouvrir le fichier!\n");
+      exit(13);
+      }
+    
+    fprintf(fdict, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n\n");
+    fprintf(fdict, "<xrec_DataDictionnary>\n");
+    
+    }
+  
+}
+
+
+void f77name(closexmldict)()
+{
+  fprintf(fdict, "</xrec_DataDictionnary>\n");
+  fclose(fdict);
+}
+
+void f77name(initvar)(nomVar, idVar, unitesVar, paletteVar, echelleVar, indDef, intVar, nbIntVar, 
+		      nomVarLen, idVarLen, unitesVarLen, paletteVarLen)
+char nomVar[];
+char idVar[];
+char unitesVar[];
+char paletteVar[];
+float *echelleVar;
+int   *indDef;
+float  intVar[][24];
+int   *nbIntVar;
+int   nomVarLen, idVarLen, unitesVarLen, paletteVarLen;
+{
+   int i,j, indDict;
+   int nbMenuItems;
+   char tab[4];
+   
+   
+   nomVar[nomVarLen-1] = '\0';
+   idVar[idVarLen-1] = '\0';
+   unitesVar[unitesVarLen-1] = '\0';
+   paletteVar[paletteVarLen-1] = '\0';
+
+   nettoyer(nomVar);
+   nettoyer(idVar);
+   nettoyer(unitesVar);
+   nettoyer(paletteVar);
+   strcpy(tab, "   ");
+
+   fprintf(fdict, "<metvar>\n");
+   fprintf(fdict, "%s <nomvar>%s</nomvar>\n", tab, nomVar);
+   fprintf(fdict, "%s <description>\n", tab);
+   fprintf(fdict, "%s %s <francais>%s</francais>\n", tab, tab, idVar);
+   fprintf(fdict, "%s %s <english>%s</english>\n", tab, tab, idVar);
+   fprintf(fdict, "%s </description>\n", tab);
+   fprintf(fdict, "%s <units>%s</units>\n", tab,unitesVar);
+   fprintf(fdict, "%s <color_palette>\n");
+   fprintf(fdict, "%s %s <palette>default</palette>\n", tab, tab);
+   fprintf(fdict, "%s %s <palette_min>0.0</palette_min>\n", tab, tab);
+   fprintf(fdict, "%s %s <palette_max>1.0</palette_max>\n", tab, tab);
+   fprintf(fdict, "%s %s <palette_variation>linear</palette_variation>\n", tab, tab);
+   fprintf(fdict, "%s </color_palette>\n", tab);
+   fprintf(fdict, "%s <min>auto</min>\n", tab);
+   fprintf(fdict, "%s <max>auto</max>\n", tab);
+   fprintf(fdict, "%s <scaling_factor>%g</scaling_factor>\n", tab, *echelleVar);
+
+   j = 0;
+   while (nbIntVar[j] > 0)
+      {
+      j++;
+      }
+
+   nbMenuItems = j;
+   
+   fprintf(fdict, "%s <default_contour>%g</default_contour>\n", tab, intVar[0][*indDef]);
+
+   for (j=0; j < nbMenuItems; j++)
+     {
+     if (j != *indDef)
+       {
+       fprintf(fdict, "%s <contour>", tab);
+       for (i=0; i < nbIntVar[j]; i++)
+	 {
+	 fprintf(fdict, " %g", intVar[i][j]);
+	 }
+       fprintf(fdict, "</contour>\n");    
+       }
+     }
+
+   fprintf(fdict, "</metvar>\n\n");
+
+   }
+
+
+nettoyer(chaine)
+char chaine[];
+{
+   int longueur;
+
+   longueur = strlen(chaine)-1;
+   while (chaine[longueur] == ' ' || chaine[longueur] == '\0')
+      {
+      chaine[longueur] = '\0';
+      longueur--;
+      }
+   }
+
+
+
+
---
+++ convert-dict/grille.c
@@ -0,0 +1,239 @@
+/* RMNLIB - Library of useful routines for C and FORTRAN programming
+ * Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
+ *                          Environnement Canada
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <rec.h>
+#include <rpnmacros.h>
+#include <memory.h>
+#include <math.h>
+#include <wgl.h>
+
+extern FILE *fdict;
+
+f77name(c_xrecgrille)(gridname,gridtype,ni,nj,ig1,ig2,ig3,ig4,lennomgrille)
+char gridname[];
+int *gridtype, *ni, *nj, *ig1, *ig2, *ig3, *ig4, lennomgrille;
+{
+  char nomgrille[64];
+  int un = 1;
+  int i, dimnomgrille;
+  int *itemp;
+  float *fig1, *fig2, *fig3, *fig4;
+  char *tab = "   ";
+  char hemisphere[16];
+  
+  fprintf(fdict, "<grid>\n");
+  strncpy(nomgrille, gridname,lennomgrille);
+  strclean(nomgrille);
+
+   fig1 = (float *) ig1;
+   fig2 = (float *) ig2;
+   fig3 = (float *) ig3;
+   fig4 = (float *) ig4;
+   
+   fprintf(fdict, "%s<ident>%s</ident>\n", tab, nomgrille);
+   switch (*gridtype)
+      {
+      case 1:
+	fprintf(fdict, "%s<grtyp>A</grtyp>\n", tab);
+	fprintf(fdict, "%s<ni>%d</ni>\n", tab, *ni);
+	fprintf(fdict, "%s<nj>%d</nj>\n", tab, *nj);
+	fprintf(fdict, "%s<ig1>%d</ig1>\n", tab, *ig1);
+	fprintf(fdict, "%s<ig2>%d</ig2>\n", tab, 0);
+	fprintf(fdict, "%s<ig3>%d</ig3>\n", tab, 0);
+	fprintf(fdict, "%s<ig4>%d</ig4>\n", tab, 0);
+	switch (*ig1)
+	  {
+	  case 0:
+	    strcpy(hemisphere, "global");
+	    break;
+	    
+	  case 1:
+	    strcpy(hemisphere, "north");
+	    break;
+	    
+	  case 2:
+	    strcpy(hemisphere, "south");
+	    break;
+	  }
+	fprintf(fdict, "%s<hemisphere>%s</hemisphere>\n", tab, hemisphere);
+        break;	
+
+        
+      case 2:
+	fprintf(fdict, "%s<grtyp>L</grtyp>\n", tab);
+	fprintf(fdict, "%s<ni>%d</ni>\n", tab, *ni);
+	fprintf(fdict, "%s<nj>%d</nj>\n", tab, *nj);
+	fprintf(fdict, "%s<swlat>%f</swlat>\n", tab, *fig1);
+	fprintf(fdict, "%s<swlon>%f</swlon>\n", tab, *fig2);
+	fprintf(fdict, "%s<dlat>%f</dlat>\n", tab, *fig3);
+	fprintf(fdict, "%s<dlon>%f</dlon>\n", tab, *fig4);
+    break;
+        
+      case 3:
+	fprintf(fdict, "%s<grtyp>N</grtyp>\n", tab);
+	fprintf(fdict, "%s<ni>%d</ni>\n", tab, *ni);
+	fprintf(fdict, "%s<nj>%d</nj>\n", tab, *nj);
+	fprintf(fdict, "%s<pi>%f</pi>\n", tab, *fig1);
+	fprintf(fdict, "%s<pj>%f</pj>\n", tab, *fig2);
+	fprintf(fdict, "%s<d60>%f</d60>\n", tab, *fig3);
+	fprintf(fdict, "%s<dgrw>%f</dgrw>\n", tab, *fig4);
+        break;
+        
+      case 4:
+	fprintf(fdict, "%s<grtyp>S</grtyp>\n", tab);
+	fprintf(fdict, "%s<ni>%d</ni>\n", tab, *ni);
+	fprintf(fdict, "%s<nj>%d</nj>\n", tab, *nj);
+	fprintf(fdict, "%s<pi>%f</pi>\n", tab, *fig1);
+	fprintf(fdict, "%s<pj>%f</pj>\n", tab, *fig2);
+	fprintf(fdict, "%s<d60>%f</d60>\n", tab, *fig3);
+	fprintf(fdict, "%s<dgrw>%f</dgrw>\n", tab, *fig4);
+        break;
+        
+      case 5:
+	fprintf(fdict, "%s<grtyp>G</grtyp>\n", tab);
+	fprintf(fdict, "%s<ni>%d</ni>\n", tab, *ni);
+	fprintf(fdict, "%s<nj>%d</nj>\n", tab, *nj);
+	fprintf(fdict, "%s<ig1>%d</ig1>\n", tab, *ig1);
+	fprintf(fdict, "%s<ig2>%d</ig2>\n", tab, 0);
+	fprintf(fdict, "%s<ig3>%d</ig3>\n", tab, 0);
+	fprintf(fdict, "%s<ig4>%d</ig4>\n", tab, 0);
+	switch (*ig1)
+	  {
+	  case 0:
+	    strcpy(hemisphere, "global");
+	    break;
+	    
+	  case 1:
+	    strcpy(hemisphere, "north");
+	    break;
+	    
+	  case 2:
+	    strcpy(hemisphere, "south");
+	    break;
+	  }
+	fprintf(fdict, "%s<hemisphere>%d</hemisphere>\n", tab, hemisphere);
+        break;
+        
+      case 6:
+	fprintf(fdict, "%s<grtyp>B</grtyp>\n", tab);
+	fprintf(fdict, "%s<ni>%d</ni>\n", tab, *ni);
+	fprintf(fdict, "%s<nj>%d</nj>\n", tab, *nj);
+	fprintf(fdict, "%s<ig1>%d</ig1>\n", tab, *ig1);
+	fprintf(fdict, "%s<ig2>%d</ig2>\n", tab, 0);
+	fprintf(fdict, "%s<ig3>%d</ig3>\n", tab, 0);
+	fprintf(fdict, "%s<ig4>%d</ig4>\n", tab, 0);
+	switch (*ig1)
+	  {
+	  case 0:
+	    strcpy(hemisphere, "global");
+	    break;
+	    
+	  case 1:
+	    strcpy(hemisphere, "north");
+	    break;
+	    
+	  case 2:
+	    strcpy(hemisphere, "south");
+	    break;
+	  }
+	fprintf(fdict, "%s<hemisphere>%d</hemisphere>\n", tab, hemisphere);
+        break;
+        
+      case 8:
+      case 10:
+      case 11:
+	fprintf(fdict, "%s<grtyp>T</grtyp>\n", tab);
+	fprintf(fdict, "%s<ni>%d</ni>\n", tab, *ni);
+	fprintf(fdict, "%s<nj>%d</nj>\n", tab, *nj);
+	fprintf(fdict, "%s<td60>%f</td60>\n", tab, *fig1);
+	fprintf(fdict, "%s<tdgrw>%f</tdgrw>\n", tab, *fig2);
+	fprintf(fdict, "%s<clat>%f</clat>\n", tab, *fig3);
+	fprintf(fdict, "%s<clon>%f</clon>\n", tab, *fig4);
+        break;
+        
+      case 9:
+        break;
+        
+      case 0:
+	fprintf(fdict, "%s<grtyp>G</grtyp>\n", tab);
+	fprintf(fdict, "%s<ni>%d</ni>\n", tab, *ni);
+	fprintf(fdict, "%s<nj>%d</nj>\n", tab, *nj);
+	fprintf(fdict, "%s<ig1>%d</ig1>\n", tab, *ig1);
+	fprintf(fdict, "%s<ig2>%d</ig2>\n", tab, 0);
+	fprintf(fdict, "%s<ig3>%d</ig3>\n", tab, 0);
+	fprintf(fdict, "%s<ig4>%d</ig4>\n", tab, 0);
+	switch (*ig1)
+	  {
+	  case 0:
+	    strcpy(hemisphere, "global");
+	    break;
+	    
+	  case 1:
+	    strcpy(hemisphere, "north");
+	    break;
+	    
+	  case 2:
+	    strcpy(hemisphere, "south");
+	    break;
+	  }
+	fprintf(fdict, "%s<hemisphere>%d</hemisphere>\n", tab, hemisphere);
+        break;
+        
+      }
+
+
+   fprintf(fdict, "</grid>\n");
+}
+
+
+/**
+ ******************************************************************************
+ ** Routine strclean
+ ** Objet:  Enleve les espaces entourant un string
+ ** Ex:     "   Skippy le kangourou   " -> "Skippy le kangourou"
+ ******************************************************************************
+ **/
+
+int strclean(str)
+char str[];
+{
+   short i, j, iinit, jinit;
+
+   iinit = 0;
+   i = iinit;
+   while (str[i] == ' ' && i < strlen(str))
+      i++;
+
+   if (i != iinit)
+      strcpy(str, str+i);
+
+   jinit = strlen(str)-1;
+   j = jinit;
+
+   while (str[j] == ' ' && j >= 0)
+      j--;
+
+   if (j != jinit)
+      str[j+1] = '\0';
+   }
+/**
+******************************************************************************
+******************************************************************************
+**/
