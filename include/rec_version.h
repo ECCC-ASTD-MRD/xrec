19b0bca09b0ccb5d79daf1f452a0b037e20b6472
Yves Chartier <Yves.Chartier@ec.gc.ca>
2014-06-05 11:09:15 +0000

    Correction du common utilise dans rtnozsrt pour definir correctement le nom d'un fichier edite (edition de champs)

--- include/rec_version.h
+++ include/rec_version.h
@@ -1,4 +1,4 @@
-static char *recVersion[] = {"Rever En Couleurs v. 5.10.1 - 04 juin 2014","Rever En Couleurs v. 5.10.1 - June 04th, 2014"};
+static char *recVersion[] = {"Rever En Couleurs v. 5.10.2 - 05 juin 2014","Rever En Couleurs v. 5.10.2 - June 05th, 2014"};
 
 static char *copyright[] = {"\nCopyright:\nDiv. de Recherche en Prevision Numerique\nEnvironnement Canada, Dorval, Quebec\n\n", 
           "\nCopyright:\nDiv. de Recherche en Prevision Numerique\nEnvironnement Canada, Dorval, Quebec\n\n"};
